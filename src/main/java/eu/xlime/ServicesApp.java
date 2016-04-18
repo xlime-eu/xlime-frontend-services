fcfac201cbfd5b8d5cf09f287af0a22f1f0b46b8
Ronald Denaux <rdenaux@expertsystem.com>
2016-04-18 16:47:27 +0200

    Bumped to 0.0.4-SNAPSHOT, added Nuria García as developer, removed verbose jersey headers.

--- src/main/java/eu/xlime/ServicesApp.java
+++ src/main/java/eu/xlime/ServicesApp.java
@@ -9,8 +9,8 @@ public class ServicesApp extends ResourceConfig {
 
 	public ServicesApp() {
 		packages("eu.xlime", "eu.xlime.bean");
-		addProperties(ImmutableMap.<String, Object>of("jersey.config.server.tracing.type", "ALL",
-				"jersey.config.server.tracing.threshold", "TRACE"));
+//		addProperties(ImmutableMap.<String, Object>of("jersey.config.server.tracing.type", "ALL",
+//				"jersey.config.server.tracing.threshold", "TRACE"));
 		register(JacksonObjectMapperProvider.class);
 		register(JacksonFeature.class);
 	}
